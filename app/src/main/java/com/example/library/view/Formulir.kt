7ee9d4967d3756ee3591babcd7b8c903af901acb
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-06 15:40:57 +0700

    menambahkan topAppBar dengan modifier

--- app/src/main/java/com/example/library/view/Formulir.kt
+++ app/src/main/java/com/example/library/view/Formulir.kt
@@ -3,6 +3,11 @@ package com.example.library.view
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
+
 
 @Composable
 fun FormIsian(
@@ -10,6 +15,11 @@ fun FormIsian(
     OnSubmitBtnClick : () -> Unit
 ){
     Scaffold (modifier= Modifier,
-
+        topBar= {
+            TopAppBar(
+                title = {Text(text=stringResource(id= R.string.home),
+                    color = Color.White)}
+            )
+        }
         )
 }
