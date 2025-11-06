94e43937f1c6f4800951ff4cf97fa35932ae0812
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-06 16:36:19 +0700

    menambahkan function TampilData

--- app/src/main/java/com/example/library/view/TampilData.kt
+++ app/src/main/java/com/example/library/view/TampilData.kt
@@ -1,10 +1,23 @@
 package com.example.library.view
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.runtime.R
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+
+
+
 
 @Composable
 fun TampilData(
     onBackBtnClick:()->Unit
 ){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin),"Lainya"),
+        Pair(stringResource(id = "ALAMAT"),"Yogyakarta"),
+    )
 
-}
