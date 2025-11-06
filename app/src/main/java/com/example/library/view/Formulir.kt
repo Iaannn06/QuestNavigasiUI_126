7d6cde70928ff0ee964f433b5afcd74e7a0144e3
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-06 15:45:08 +0700

    menambahkan column

--- app/src/main/java/com/example/library/view/Formulir.kt
+++ app/src/main/java/com/example/library/view/Formulir.kt
@@ -1,5 +1,7 @@
 package com.example.library.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,5 +27,9 @@ fun FormIsian(
                     (containerColor = colorResource(id = R.color.teal_700))
             )
         }
-        )
+        ){isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues=isiRuang),
+            )
+
+    }
 }
