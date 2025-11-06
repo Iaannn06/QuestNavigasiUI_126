f07ac6231a699e5fd5335886afffe206b6922528
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-11-06 15:49:33 +0700

    menambahkan OutlinedTextField

--- app/src/main/java/com/example/library/view/Formulir.kt
+++ app/src/main/java/com/example/library/view/Formulir.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.material3.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.ui.Alignment
+import androidx.compose.material3.OutlinedTextField
 
 @Composable
 fun FormIsian(
@@ -32,7 +33,12 @@ fun FormIsian(
         ){isiRuang ->
         Column(modifier = Modifier.padding(paddingValues=isiRuang),
            verticalArrangement = Arrangement.Top,
-           horizontalAlignment = Alignment.CenterHorizontally )
+           horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            OutlinedTextField(
+
+            )
+        }
 
     }
 }
